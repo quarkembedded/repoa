8b4e45efb226eb5707ae13346e9ff6071d7f8f4f
Victor Ghosh <victor.ghosh@quarkembedded.com.au>
2022-02-01 16:16:27 +0800

    Just

--- t1/main.c
+++ t1/main.c
@@ -5,12 +5,8 @@ int main(int argc, char *argv[])
 	printf("This is Victor Ghosh\n");
 	return 0;
 }
-<<<<<<< HEAD
-Hello
-=======
 
 int some_featurex_function(int a)
 {
 	return 0;
 }
->>>>>>> featureX
